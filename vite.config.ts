58ae443da1225072ea852c9acd2e7be320df95ab
wainerrodriguez <weiner.rodriguez@costaricasoftwareservices.com>
2024-04-24 20:20:31 -0600

    Added vite test conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["jest"],
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    exclude: [
+      ...configDefaults.exclude,
+      "./src/theme.ts",
+      "**/src/*App.tsx",
+      "*/main.tsx",
+    ],
+    environment: "jsdom",
+    coverage: {
+      provider: "istanbul", // or 'v8'
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
     },
   },
 });
